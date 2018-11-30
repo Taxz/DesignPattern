326cb85e5aec6bf6d5cd8c011a5bf38d5ad8b8ef
Taxz <1240887908@qq.com>
2018-11-30 09:38:44 +0800

    Update IMyIterator.java

--- src/main/java/study/txz/example/iterator/IMyIterator.java
+++ src/main/java/study/txz/example/iterator/IMyIterator.java
@@ -3,6 +3,7 @@ package study.txz.example.iterator;
 /**
  * Created by Taxz on 2018/11/29/029.
  * GitHub:https://github.com/Taxz
+ *      容器接口类
  */
 public interface IMyIterator {
         study.txz.example.iterator.Iterator createIterator();
