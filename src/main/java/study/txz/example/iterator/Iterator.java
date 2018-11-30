677f74ba2420cb6cfcfea9bceb25be422e52902c
Taxz <1240887908@qq.com>
2018-11-30 09:35:06 +0800

    Update Iterator.java

--- src/main/java/study/txz/example/iterator/Iterator.java
+++ src/main/java/study/txz/example/iterator/Iterator.java
@@ -3,8 +3,10 @@ package study.txz.example.iterator;
 /**
  * Created by Taxz on 2018/11/29/029.
  * GitHub:https://github.com/Taxz
+ *  迭代器接口
  */
 public interface Iterator<Iteam> {
+   
     Iteam next();
 
     boolean hasNext();
